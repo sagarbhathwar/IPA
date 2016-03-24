607a323381b060d49dd5f9ab9ab9ea0c66104e7b
Sagar B Hathwar <sagarbhathwar@gmail.com>
2016-03-24 11:51:31 +0530

    Bug fix
    Fixed bug wherein the addition didn't work when the size of one operand was greater than the other but size of result exceeded that of the largest operand

--- addition.h
+++ addition.h
@@ -3,8 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef char* ipz_t;
 typedef char digit;
+typedef char* ipz_t;
+
+void ipz_prepend_zeroes(ipz_t number, int n)
+{
+	ipz_t __tmp = calloc(n + strlen(number) + 1, sizeof(char));
+	memset(__tmp, '0', n);
+	strcat(__tmp, number);
+	memcpy(number, __tmp, n + strlen(number));
+	free(__tmp);
+}
 
 ipz_t ipz_add(ipz_t op1, ipz_t op2)
 {
@@ -12,6 +21,28 @@ ipz_t ipz_add(ipz_t op1, ipz_t op2)
 	int l2 = strlen(op2);
 	int c = 0;
 	ipz_t result;
+	if(op1[0] == '9' || op2[0] == 9)
+	{
+		ipz_t __tmp1 = calloc(strlen(op1), sizeof(char));
+		ipz_t __tmp2 = calloc(strlen(op1), sizeof(char));
+		strcpy(__tmp1, op1);
+		strcpy(__tmp2, op2);
+		strlen(op1) > strlen(op2) ? ipz_prepend_zeroes(__tmp2, strlen(op1) - strlen(op2)) : ipz_prepend_zeroes(__tmp1, strlen(op2) - strlen(op1));
+		result = calloc(strlen(__tmp1) + 1, sizeof(char));
+		memset(result, '0', strlen(__tmp1) + 1);
+		int c = 0;
+		for(int i = strlen(__tmp1) - 1; i >= 0; --i)
+		{
+			int t = (__tmp1[i] - '0') + (__tmp2[i] - '0') + c;
+			c = t / 10;
+			result[i + 1] = (t % 10) + '0' ;
+		}
+		if(c != 0)
+			result[0] = c + '0';
+		else
+			result++;
+		return result;
+	}
 	if(l1 > l2)
 	{
 		result = calloc(l1, sizeof(digit));
@@ -54,5 +85,4 @@ ipz_t ipz_add(ipz_t op1, ipz_t op2)
 	}
 	return result;
 }
-
 #endif
