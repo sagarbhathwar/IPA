f7be93e560bbc8fd37b50030b67644677128571c
Sagar B Hathwar <sagarbhathwar@gmail.com>
2016-04-23 17:59:52 +0530

    Update ipz.h

--- ipz.h
+++ ipz.h
@@ -21,9 +21,9 @@ void ipz_printf(ipz_t x);
 
 void ipz_add(ipz_t sum, ipz_t augend, ipz_t addend);
 
-void ipz_sub(ipz_t difference, ipz_t minuent, ipz_t subtrahend);
+void ipz_sub(ipz_t difference, ipz_t minuend, ipz_t subtrahend);
 
-void ipz_mul(ipz_t product, ipz_t multiplicamd, ipz_t multiplier);
+void ipz_mul(ipz_t product, ipz_t multiplicand, ipz_t multiplier);
 
 void ipz_div(ipz_t quotient, ipz_t divident, ipz_t divisor);
 
