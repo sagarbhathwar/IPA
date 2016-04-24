deede5a6aa3c128b2009ad37bbb011768a81f2ef
Sagar B Hathwar <sagarbhathwar@gmail.com>
2016-04-24 19:41:51 +0530

    Unnecessary

--- util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-#include "ipz.h"
-
-void append_zeroes(ipz_t input, unsigned int n);
-void split(ipz_t input, ipz_t first_part, ipz_t second_part, int length);
-
-#endif
